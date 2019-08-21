1ebb53f1eb3c345b2e7fa005274feb8b7ca5ac20
wanghc00 <306093226@qq.com>
2019-08-21 21:06:58 +0800

    作业提交

--- __tests__/main-sepc.js
+++ __tests__/main-sepc.js
@@ -1,15 +1,11 @@
 const add = require('../main');
-const createMultilyTable = require('../mainMultilyTable');
-
-it ('should add two numbers', () => {
-    expect(add(2, 3)).toBe(5);
-});
+const createMultilyTable = require('../main');
 
 it ('should require two numbers multily table', () => {
     let firstNumber = 2;
     let secondNumber = 4;
     const result = createMultilyTable(firstNumber, secondNumber);
-    expect(result[0][0]).toBe("2*2=4");
-    expect(result[1][0]).toBe("2*3=6");
-    expect(result[2][0]).toBe("2*4=8");
+    expect(result).toBe("2*2=4\r\n" +
+                                           "2*3=6  3*3=9\r\n" +
+                                           "2*4=8  3*4=12  4*4=16");
 });
--- main.js
+++ main.js
@@ -1,5 +1,58 @@
-function add (left, right) {
-    return left + right;
+function isInt(number){
+    if(Number.isInteger(number)){
+        return 1;
+    }
+    return 0;
 }
 
-module.exports = add;
+function isStartLessThanEnd(firstNumber,secondNumber){
+    if(firstNumber < secondNumber){
+        return 1;
+    }
+    return 0;
+}
+
+function isInScope(number){
+    if(number >=1 && number <=1000){
+        return 1;
+    }
+    return 0;
+}
+
+function calculate(firstNumber, secondNumber){
+    let multiplyTable = [];
+    for(let i=firstNumber; i<=secondNumber; i++){
+        for (let j=firstNumber; j<=i; j++){
+            multiplyTable+=j+"*"+i+"="+i*j;
+            if(i!==j){
+                multiplyTable += "  "
+            }
+        }
+          if(i<secondNumber) {
+              multiplyTable+="\r\n";
+          }
+    }
+    return multiplyTable;
+}
+
+function createMultilyTable(firstNumber, secondNumber){
+
+    if(isStartLessThanEnd(firstNumber, secondNumber) == 0){
+        return null;
+    }
+
+    if(!isInScope(firstNumber) || !isInScope(secondNumber)){
+        return null;
+    }
+
+    if(!isInt(firstNumber)||!isInt(secondNumber)){
+        return null;
+    }
+    
+    var multiplyTable = calculate(firstNumber, secondNumber);
+
+    return multiplyTable;
+}
+
+
+module.exports = createMultilyTable;
